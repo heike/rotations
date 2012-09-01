a1017e964e6e52185f7e18bc058ab35fa1ac76c1
Bryan Stanfill <stanfill@iastate.edu>
2012-09-01 15:06:18 -0500

    update man

--- R/help.R
+++ R/help.R
@@ -3,6 +3,6 @@
 #' This package implements tools for working with rotational data: it allows simulation from the most commonly used  distributions on the sphere, it estimates differen mean and median type estimators for the main direction of a number of rotations, it provides (bootstrap) confidence regions for the estimates, and it allows to visualize rotational data. 
 #' 
 #' @docType package
-#' @bibliography inst/StanfillHofmannGenschel.bib
+#' @bibliography rotations/inst/StanfillHofmannGenschel.bib
 #' @name package-rotations
 NULL
